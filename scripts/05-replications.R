e36ab51060d4f5553cc71705ce170a868b80756c
Alex Lee <alexxjlee10@gmail.com>
2024-09-19 17:55:12 -0400

    Add files via upload

---
+++ scripts/05-replications.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Author: Alex Lee [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: alexxj.lee@mail.utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+
